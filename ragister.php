ef8b209b66a227ee9f3d57723580f222230a8e20
Priya8632 <pthummar775@gmail.com>
2022-03-05 22:51:13 +0530

    insert

--- ragister.php
+++ ragister.php
@@ -9,7 +9,7 @@ $query = "SELECT  * FROM STUDENT";
         $createtbl = "CREATE TABLE student(
         Id int(10) auto_increment primary key,
         Game text,
-        Full_Name text,
+        Full_name text,
         Standard text,
         Roll_no int(3),
         Mobile int(10),
@@ -31,30 +31,19 @@ if(isset($_REQUEST['submit'])){
         $fnamearr = "only character and letter ";    }
     elseif(empty($_POST['gender'])){
         $genderarr = "gender is required";}
-    elseif()
+
     else{
 
-    
+    $game =$_POST['game'];
     $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $email = $_POST['email'];
-    $pw = base64_encode($_POST['p_word']);
-    $cw = $_POST['c_word'];
+    $std = $_POST['std'];
+    $rollno = $_POST['rollno'];
+    $mob = $_POST['mob'];
     $gender = $_POST['gender'];
-    $age = $_POST['age'];
-    $dept = $_POST['dept'];
-    $doj = $_POST['doj'];
-    $sal = $_POST['sal'];
-    $chkbox = $_POST['hobby'];
-    $alldata= implode(",",$chkbox);
+    
 
-    $target_dir = "image/";
-
-    $imagepath = $target_dir.basename($_FILES['file']['name']);
-    $chkfile = move_uploaded_file($_FILES['file']['tmp_name'], $imagepath);
-
-    $insert = "INSERT INTO EMPLOYEE
-        (`fname`,`lname`,`email`,`p_word`,`c_word`,`gender`,`age`,`dept`,`doj`,`sal`,`hobby`,`img`) VALUES 
+    $insert = "INSERT INTO student
+        (`Game`,`Full_name`,`Standard`,`Roll_no`,`Mobile`,`gender`) VALUES 
         ('$fname','$lname','$email','$pw','$cw','$gender','$age','$dept','$doj','$sal','$alldata','$imagepath')";
     
         if(mysqli_query($conn,$insert)){
@@ -94,8 +83,8 @@ if(isset($_REQUEST['submit'])){
         <form action="">
             <div class="gorm-group">
                 <label>GAME</label>
-                <select name="" id="" class="form-control">
-                    <option value="">---select---</option>
+                <select name="" id="" class="form-control" name="game"> 
+                    <option value="">---select---</option> 
                     <option value="chess">CHESS</option>
                     <option value="khokho">KHOKHO</option>
                     <option value="cricket">CRICKET</option>
