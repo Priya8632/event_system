fcd179014afd6297471ae105ac2ae5cf060ddfc1
Priya8632 <pthummar775@gmail.com>
2022-03-06 10:41:23 +0530

    addition

--- ragister.php
+++ ragister.php
@@ -3,17 +3,20 @@
 include 'conn.php';
 $query = "SELECT  * FROM STUDENT";
 
+$fnamearr = $genderarr = $stdarr = $rollarr = $mobarr = $gamearr ="";
+$game =$fname = $std = $mob =$rollno = $gender = "";
 
+if(isset($_REQUEST['submit'])){
     if(!mysqli_query($conn,$query)){
     
         $createtbl = "CREATE TABLE student(
         Id int(10) auto_increment primary key,
         Game text,
         Full_name text,
-        Standard text,
+        Standard int(3),
         Roll_no int(3),
         Mobile int(10),
-        gender text
+        Gender text
     )";
     $tblchk = mysqli_query($conn,$createtbl);
     if(!$tblchk){
@@ -21,16 +24,26 @@ $query = "SELECT  * FROM STUDENT";
     }
     }
 
-if(isset($_REQUEST['submit'])){
-
-
-
-    if(empty($_POST['fname'])){
+    if(empty($_POST['game'])){
+        $gamearr = "required";}
+    elseif(empty($_POST['fname'])){
         $fnamearr = "fname is required";}
     elseif(!preg_match("/^[a-zA-Z-' ]*$/",$_POST['fname'])){
         $fnamearr = "only character and letter ";    }
     elseif(empty($_POST['gender'])){
         $genderarr = "gender is required";}
+    elseif(empty($_POST['std'])){
+        $stdarr = "standard is required";}
+    elseif(!preg_match("/[0-9]/",$_POST['std'])){
+        $stdarr = "only number of std"; }
+    elseif(empty($_POST['mob'])){
+        $mobarr = "mobile number is required";}
+    elseif(!preg_match("/[0-9]/",$_POST['mob'])){
+        $mobarr = "only mobile no"; }
+    elseif(empty($_POST['rollno'])){
+        $rollarr = "rollno number is required";}
+    elseif(!preg_match("/[0-9]/",$_POST['rollno'])){
+        $rollarr = "only rollno number"; }
 
     else{
 
@@ -43,11 +56,11 @@ if(isset($_REQUEST['submit'])){
     
 
     $insert = "INSERT INTO student
-        (`Game`,`Full_name`,`Standard`,`Roll_no`,`Mobile`,`gender`) VALUES 
-        ('$fname','$lname','$email','$pw','$cw','$gender','$age','$dept','$doj','$sal','$alldata','$imagepath')";
+        (`Game`,`Full_name`,`Standard`,`Roll_no`,`Mobile`,`Gender`) VALUES 
+        ('$game','$fname','$std','$rollno','$mob','$gender')";
     
         if(mysqli_query($conn,$insert)){
-            header('location:user_login.php');
+            header('location:message.php');
         } 
     }
 }
@@ -80,41 +93,54 @@ if(isset($_REQUEST['submit'])){
 <body>
     <div class="container">
         <h2>Ragister Form</h2>
-        <form action="">
+        <form action="" method="POST">
+
             <div class="gorm-group">
                 <label>GAME</label>
-                <select name="" id="" class="form-control" name="game"> 
-                    <option value="">---select---</option> 
+                <select  class="form-control" name="game"> 
+                    <option value=""><?php if(isset($_POST['game'])) { echo $_POST['game'];}?></option> 
                     <option value="chess">CHESS</option>
                     <option value="khokho">KHOKHO</option>
                     <option value="cricket">CRICKET</option>
                     <option value="football">FOOTBALL</option>
-                </select>
+                </select> <span class="error">* <?php echo $gamearr;?>
             </div>
+
             <div class="form-group">
                 <label>Full Name</label>
-                <input type="text" class="form-control" name="fname">
+                <input type="text" class="form-control" name="fname" value="<?php if(isset($_POST['fname'])) { echo $_POST['fname'];}?>">
+                <span class="error">* <?php echo $fnamearr;?>
             </div>
+
             <div class="form-group">
                 <label>Standard</label>
-                <input type="text" class="form-control" name="std">
+                <input type="text" class="form-control" name="std" value="<?php if(isset($_POST['std'])) { echo $_POST['std'];}?>">
+                <span class="error">* <?php echo $stdarr;?>   
             </div>
+
             <div class="form-group">
                 <label>Roll No</label>
-                <input type="text" class="form-control" name="rollno">
+                <input type="text" class="form-control" name="rollno" value="<?php if(isset($_POST['rollno'])) { echo $_POST['rollno'];}?>">
+                <span class="error">* <?php echo $rollarr;?>
             </div>
+
             <div class="form-group">
                 <label>Mobile No</label>
-                <input type="text" class="form-control" name="mobileno">
+                <input type="text" class="form-control" name="mob" value="<?php if(isset($_POST['mob'])) { echo $_POST['mob'];}?>">
+                <span class="error">* <?php echo $mobarr;?>
             </div>
+
             <div class="form-group">
                 <label>Gender</label>
                 <input type="radio" name="gender" value="male" <?php if(isset($_POST['gender']) == 'male') { echo 'checked';}?>>MALE
                 <input type="radio" name="gender" value="female" <?php if(isset($_POST['gender']) == 'female') { echo 'checked';}?>>FEMALE
+                <span class="error">* <?php echo $genderarr;?>
             </div>
+
             <div class="form-group">
                 <button type="submit" class="btn btn-primary" name="submit">SUBMIT</button>
             </div>
+
         </form>
     </div>
 </body>
